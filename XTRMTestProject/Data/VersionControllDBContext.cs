76a27e8fa8eb4ea8a0e9d9830ec797a9cd2502d0
Arkadiy Parinov <arkady.khv@mail.ru>
2015-07-01 22:32:52 +1000

    Do something with EF DbContext

---
+++ XTRMTestProject/Data/VersionControllDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XTRMTestProject.Model;
+
+namespace XTRMTestProject.Data
+{
+    public class VersionControllContext: DbContext
+    {
+        public VersionControllContext(): base("VersionControllDB")
+        {
+        }
+
+        public DbSet<ControlledClass> ControlledClasses { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<XTRMTestProject.Model.Version> Versions { get; set; }
+    }
+}
--- XTRMTestProject/Version Control/VersionController.cs
+++ XTRMTestProject/Version Control/VersionController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using XTRMTestProject.Data;
 
 namespace XTRMTestProject.Version_Control
 {
     public class VersionController
     {
         private Assembly assembly;
+        private VersionControllContext db = new VersionControllContext(); 
 
         public VersionController()
         {
@@ -25,36 +27,36 @@ namespace XTRMTestProject.Version_Control
         // Return TRUE when some changes are exist and FALSE when they does not
         public bool SearchAndAddNewVersions()
         {
-
+            return false;
         }
 
         // Finde all classes with attribute type of attrType
         private List<Type> GetTypesWithAttribute(Type attrType)
         {
-            List<Type> result = new List<Type>();
+            List<Type> classesList = new List<Type>();
             foreach (Type type in assembly.GetTypes())
             {
                 if (type.GetCustomAttributes(attrType, true).Length > 0)
                 {
-                    result.Add(type);
+                    classesList.Add(type);
                 }
             }
-            return result;
+            return classesList;
         }
 
         // Finde all methods of class with type classType and attribute attrType
         private List<MethodInfo> FindMethodsWithAttribute(Type classType, Type attrType)
         {
+            List<MethodInfo> methodList = new List<MethodInfo>();
             var methods = classType.GetMethods();
-            List<MethodInfo> result = new List<MethodInfo>();
             foreach (var method in methods)
             {
                 var attributes = method.GetCustomAttributes(attrType, true);
                 if (attributes != null && attributes.Length > 0)
-                    result.Add(method);
+                    methodList.Add(method);
             }
 
-            return result;
+            return methodList;
         }
     }
 }
