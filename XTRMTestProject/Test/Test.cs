48a846fe7942f5e022235aa4d4228b801a5a2ead
Arkadiy Parinov <arkady.khv@mail.ru>
2015-07-05 17:10:58 +1000

    Add version viewer with test

---
+++ XTRMTestProject/Test/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using XTRMTestProject.Version_Control;
+
+namespace XTRMTestProject.Test
+{
+
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public static void TestDiffMethod()
+        {
+            VersionViewer vv = new VersionViewer();
+
+            List<string> Old = new List<string>(){"1","2","8","3","6", "7"};
+
+            List<string> New = new List<string>() { "1", "2", "3", "4", "5", "6" };
+
+            // To test this change method GetDifferenceBetweenTwoLists to public and uncomment next 2 code lines
+            //var result = vv.GetDifferenceBetweenTwoLists(New, Old);
+
+            //Assert.AreEqual(result, new List<String>() {"1", "2", "-8", "3", "+4", "+5", "6", "-7"});
+        }
+    }
+}
---
+++ XTRMTestProject/Version Control/VersionViewer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roslyn.Compilers;
+using XTRMTestProject.Data;
+using XTRMTestProject.Model;
+
+namespace XTRMTestProject.Version_Control
+{
+    public class VersionViewer
+    {
+        private VersionControllContext db;
+
+        public VersionViewer()
+        {
+            this.db = new VersionControllContext();
+        }
+
+        public List<ControlledClass> GetAllControlledClasses()
+        {
+            return db.ControlledClasses.ToList();
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return db.Users.ToList();
+        }
+
+        public List<Model.Version> GetVersionsForClass(int classID)
+        {
+            return db.Versions.Where(v=>v.controlledClass.id == classID).ToList();
+        }
+
+        public List<Model.Version> GetVersionsByUser(int userID)
+        {
+            return db.Versions.Where(v => v.user.id == userID).ToList();
+        }
+
+        public List<string> GetVersionSourceCode(Model.Version version)
+        {
+            var newVersionCode = version.classBody.Split(new string[] { "[~]" }, StringSplitOptions.None).ToList();
+
+            if (version.previousVersion == null)
+            {
+                return newVersionCode;
+            }
+            else
+            {
+                var oldVersionCode = version.previousVersion.classBody.Split(new string[] { "[~]" }, StringSplitOptions.None).ToList();
+                return GetDifferenceBetweenTwoLists(newVersionCode,oldVersionCode);
+            }
+        }
+
+        // Change to private (public in tests only)
+        private  List<string> GetDifferenceBetweenTwoLists(List<string> newVersion, List<string> oldVersion)
+        {
+            List<string> result = new List<string>();
+
+            var deletedItems = oldVersion.Except(newVersion).ToList();
+
+            var addedItems = newVersion.Except(oldVersion).ToList();
+
+            // Use j - index to work with old list, i - index - with new list
+            int i = 0;
+            int j = 0;
+            for (int step = 0; step < oldVersion.Count + deletedItems.Count + addedItems.Count ; step++)
+            {
+                if (i < newVersion.Count && j < oldVersion.Count && newVersion[i] == oldVersion[j])
+                {
+                    result.Add(newVersion[i]);
+                    if (i < newVersion.Count){i++;}
+                    if (j < oldVersion.Count){j++;}
+                }
+
+                else
+                {
+                    if (j < oldVersion.Count)
+                    {
+                        if (deletedItems.FindIndex(f => f == oldVersion[j]) >= 0)
+                        {
+                            result.Add(string.Format("-{0}", oldVersion[j]));
+                           j++;
+                        }
+                    }
+
+                    if (i < newVersion.Count)
+                    {
+                        if (addedItems.FindIndex(f => f == newVersion[i]) >= 0)
+                        {
+                            result.Add(string.Format("+{0}", newVersion[i]));
+                            i++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
