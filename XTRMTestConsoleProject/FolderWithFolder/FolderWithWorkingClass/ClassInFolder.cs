9280587ca92294143d0aaa378c13788cdbbd21b4
Arkadiy Parinov <arkady.khv@mail.ru>
2015-07-05 14:05:46 +1000

    Add multiple folders with working class to project

---
+++ XTRMTestConsoleProject/FolderWithFolder/FolderWithWorkingClass/ClassInFolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XTRMTestProject;
+
+namespace XTRMTestConsoleProject.FolderWithFolder.FolderWithWorkingClass
+{
+    [VersionControl("Developer_1", "03.07.2015", "Add new class to project", "ClassInFolder.cs")]
+    class ClassInFolder
+    {
+    }
+}
